7d67353b8b8e31ee9fafcf22da05002c8f9829d5
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2018-11-24 03:42:05 -0200

    modif

--- PRICExDEV/r_script.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-library(babynames)
-library(streamgraph)
-
-babynames %>%
-  filter(grepl("^Kr", name)) %>%
-  group_by(year, name) %>%
-  tally(wt=n) %>%
-  streamgraph("name", "n", "year")
